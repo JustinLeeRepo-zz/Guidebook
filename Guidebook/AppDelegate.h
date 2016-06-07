2942de2d1572ab0c0251ed5e652690b30a85d9f4
Justin Lee <justinj.lee23@gmail.com>
2016-06-07 15:22:32 -0700

    Initial Commit

---
+++ Guidebook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guidebook
+//
+//  Created by Justin Lee on 6/7/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Guidebook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Guidebook
+//
+//  Created by Justin Lee on 6/7/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
